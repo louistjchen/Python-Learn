758429ca6a0d48292d8fa28980a858536765556e
Louis Chen <louischen@Louis-Chen.local>
2017-08-06 00:23:36 -0400

    update web_xml.py to have break function

--- web_xml.py
+++ web_xml.py
@@ -1,18 +1,20 @@
 import urllib.request, urllib.parse, urllib.error
 import xml.etree.ElementTree as ET
 
-url = input('Enter location: ')
+while True:
+    url = input('Enter location: ')
+    if(len(url) < 1): break
 
-print('Retrieving', url)
-uh = urllib.request.urlopen(url)
-data = uh.read()
-print('Retrieved', len(data), 'characters')
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url)
+    data = uh.read()
+    print('Retrieved', len(data), 'characters')
 
-tree = ET.fromstring(data.decode())
-counts = tree.findall('.//count')
-print('count:', len(counts))
+    tree = ET.fromstring(data.decode())
+    counts = tree.findall('.//count')
+    print('count:', len(counts))
 
-total = 0
-for count in counts:
-    total = total + int(count.text)
-print('sum', total)
+    total = 0
+    for count in counts:
+        total = total + int(count.text)
+    print('sum', total)
